2736350174be5fc1cc5ca9821556902357846ccc
nyoko708 <ckgxd325@yahoo.co.jp>
2015-11-10 22:23:52 +0900

    Initial Commit

---
+++ test-streamingTests/test_streamingTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_streamingTests.swift
+//  test-streamingTests
+//
+//  Created by 横田直也 on 2015/11/10.
+//  Copyright © 2015年 横田直也. All rights reserved.
+//
+
+import XCTest
+@testable import test_streaming
+
+class test_streamingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
